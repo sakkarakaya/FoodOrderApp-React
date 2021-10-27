bc0ab850d192a60cd1357dbb715a1781b5aaf710
sakkarakaya <sakkarakaya@icloud.com>
2021-10-27 15:21:31 +0200

    added cartbutton to header

---
+++ react-foodorder/src/components/Layout/CartButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CartIcon from '../Cart/CartIcon'
+import classes from './CartButton.module.css'
+
+const CartButton = (props) => {
+    return (
+        <button className={classes.button} onClick={props.onClick}>
+            <span className={classes.icon}><CartIcon /></span>
+            <span>Cart</span>
+            <span className={classes.badge}>3</span>
+        </button>
+    )
+}
+
+export default CartButton
