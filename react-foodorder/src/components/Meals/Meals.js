c36955b0de3df8c55bda0d053f51ef23f57d196e
sakkarakaya <sakkarakaya@icloud.com>
2021-11-02 10:20:40 +0100

    added meals

---
+++ react-foodorder/src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MealList from './MealList'
+import MealSummary from './MealSummary'
+
+const Meals = () => {
+    return (
+        <div>
+            <MealSummary />
+            <MealList />
+        </div>
+    )
+}
+
+export default Meals
