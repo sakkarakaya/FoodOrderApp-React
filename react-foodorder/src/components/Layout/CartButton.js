b0e5bad47d47e56872545ee4218d036ac04b26c3
sakkarakaya <sakkarakaya@icloud.com>
2021-11-11 08:54:59 +0100

    added context to cartbutton

--- react-foodorder/src/components/Layout/CartButton.js
+++ react-foodorder/src/components/Layout/CartButton.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import CartIcon from '../Cart/CartIcon'
 import classes from './CartButton.module.css'
+import { CartContext } from '../../store/CartContext'
 
 const CartButton = (props) => {
+    const cartContext = useContext(CartContext);
+    const numberOfItemsOnCart = cartContext.items.reduce((currentAmount, item) => {
+        return currentAmount + item.amount
+    }, 0);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={classes.cartbutton} onClick={props.onClick}>
             <span className={classes.icon}><CartIcon /></span>
             <span>Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfItemsOnCart}</span>
         </button>
     )
 }
