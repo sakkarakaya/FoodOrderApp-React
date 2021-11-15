276ebdc495f072143be4cbe63b6dc5d6180bf41e
sakkarakaya <sakkarakaya@icloud.com>
2021-11-15 08:59:19 +0100

    added meal images

--- react-foodorder/src/components/Meals/MealList.js
+++ react-foodorder/src/components/Meals/MealList.js
@@ -6,27 +6,31 @@ import classes from './MealList.module.css'
 const DUMMY_MEALS = [
     {
         id: 'm1',
-        name: 'Sushi',
+        name: 'Sushi    ',
         description: 'Finest fish and veggies',
         price: 22.99,
+        img: 'https://imgur.com/NV9P8l6.jpg'
     },
     {
         id: 'm2',
         name: 'Schnitzel',
         description: 'A german specialty!',
         price: 16.5,
+        img: 'https://imgur.com/lEWyzvr.jpg'
     },
     {
         id: 'm3',
-        name: 'Barbecue Burger',
+        name: 'Burger   ',
         description: 'American, raw, meaty',
         price: 12.99,
+        img: 'https://imgur.com/iOBuPCp.jpg'
     },
     {
         id: 'm4',
-        name: 'Green Bowl',
+        name: 'GreenBowl',
         description: 'Healthy...and green...',
         price: 18.99,
+        img: 'https://imgur.com/oRDxyYD.jpg'
     },
 ];
 
