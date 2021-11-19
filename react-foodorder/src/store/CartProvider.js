25d2fe021fcc6bc741381be1e7494d5731765010
sakkarakaya <sakkarakaya@icloud.com>
2021-11-19 09:10:11 +0100

    added reducers

--- react-foodorder/src/store/CartProvider.js
+++ react-foodorder/src/store/CartProvider.js
@@ -9,16 +9,47 @@ const initialState = {
 const reducer = (state, action) => {
     switch (action.type) {
         case 'ADD':
-            const updatedItems = state.items.concat(action.item);
             const updatedTotalAmount = state.totalAmount + action.item.amount * action.item.price;
+
+            const existingItemIndex = state.items.findIndex(item => item.id === action.item.id)
+            const existingItem = state.items[existingItemIndex]
+
+            let updatedItems
+
+            if (existingItem) {
+                const updatedItem = { ...existingItem, amount: existingItem.amount + action.item.amount }
+                updatedItems = [...state.items]
+                updatedItems[existingItemIndex] = updatedItem
+            } else {
+                updatedItems = state.items.concat(action.item);
+            }
+
             return {
                 items: updatedItems,
                 totalAmount: updatedTotalAmount
             };
+
         case 'REMOVE':
-            return state;
+            const existingItemIndex2 = state.items.findIndex(item => item.id === action.id)
+            const existingItem2 = state.items[existingItemIndex2]
+            const updatedTotalAmount2 = state.totalAmount - existingItem2.price
+
+            let updatedItems2
+
+            if (existingItem2.amount === 1) {
+                updatedItems2 = state.items.filter(item => item.id !== action.id)
+            } else {
+                const updatedItem2 = { ...existingItem2, amount: existingItem2.amount - 1 }
+                updatedItems2 = [...state.items]
+                updatedItems2[existingItemIndex2] = updatedItem2
+            }
+
+            return {
+                items: updatedItems2,
+                totalAmount: updatedTotalAmount2
+            };
         default:
-            return state;
+            return initialState;
     }
 }
 
