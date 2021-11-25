f8df6e03f9bee8b8f1380af4666dd959c51996e5
sakkarakaya <sakkarakaya@icloud.com>
2021-11-25 13:33:32 +0100

    fetched data from firebase

--- react-foodorder/src/components/Meals/MealList.js
+++ react-foodorder/src/components/Meals/MealList.js
@@ -1,41 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
-import classes from './MealList.module.css'
-
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi    ',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-        img: 'https://imgur.com/NV9P8l6.jpg'
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-        img: 'https://imgur.com/lEWyzvr.jpg'
-    },
-    {
-        id: 'm3',
-        name: 'Burger   ',
-        description: 'American, raw, meaty',
-        price: 12.99,
-        img: 'https://imgur.com/iOBuPCp.jpg'
-    },
-    {
-        id: 'm4',
-        name: 'GreenBowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-        img: 'https://imgur.com/oRDxyYD.jpg'
-    },
-];
+import classes from './MealList.module.css';
+import axios from 'axios'
 
 const MealList = () => {
-    const mealsList = DUMMY_MEALS.map((item) => <MealItem key={item.id} id={item.id} item={item} />)
+
+    const [meals, setMeals] = useState([])
+
+    const fetchHandler = async () => {
+        try {
+            const { data } = await axios.get(
+                'https://react-foodorder-fa1bc-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+            )
+            const mealList = []
+            for (const key in data) {
+                mealList.push({
+                    id: key,
+                    name: data[key].title,
+                    description: data[key].desc,
+                    price: data[key].price,
+                    img: data[key].img
+                })
+            }
+            setMeals(mealList)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchHandler()
+    }, [])
+
+    const mealsList = meals.map((item) => <MealItem key={item.id} id={item.id} item={item} />)
     return (
         <section className={classes.meals}>
             <Card>
