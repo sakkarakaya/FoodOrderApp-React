acb9447d954aea8e2c4409021c3245da376663b5
sakkarakaya <sakkarakaya@icloud.com>
2021-11-26 14:50:25 +0100

    fixed value bug as defaultValue

--- react-foodorder/src/components/UI/Input.js
+++ react-foodorder/src/components/UI/Input.js
@@ -5,7 +5,7 @@ const Input = React.forwardRef((props, ref) => {
     return (
         <div className={classes.input}>
             <label htmlFor={props.input.id}>{props.label}</label>
-            <input ref={ref} value="1" id={props.input.id} {...props} />
+            <input ref={ref} defaultValue="1" id={props.input.id} {...props} />
         </div>
     )
 })
